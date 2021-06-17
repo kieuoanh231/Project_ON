929bb1136a5150817688fff8d012deaf49e42d56
cms <80998461+kieuoanh231@users.noreply.github.com>
2021-06-18 02:17:14 +0700

    keen slider vertical

--- pages/test.js
+++ pages/test.js
@@ -1,145 +1,221 @@
 import React from "react";
 import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
 
 export default (props) => {
-  const [currentSlide, setCurrentSlide] = React.useState(0);
-  const [pause, setPause] = React.useState(false);
-  const timer = React.useRef();
-
   const [sliderRef, slider] = useKeenSlider({
-    initial: 0,
-    loop: true,
-    duration: 1000,
-    dragStart: () => {
-      setPause(true);
-    },
-    dragEnd: () => {
-      setPause(false);
-    },
-    slideChanged(s) {
-      setCurrentSlide(s.details().relativeSlide);
-      console.log(s.details().relativeSlide);
-    },
+    spacing: 10,
+    slidesPerView: 3,
+    vertical: true,
   });
-
-  React.useEffect(() => {
-    sliderRef.current.addEventListener("mouseover", () => {
-      setPause(true);
-    });
-    sliderRef.current.addEventListener("mouseout", () => {
-      setPause(false);
-    });
-  }, [sliderRef]);
-
-  React.useEffect(() => {
-    timer.current = setInterval(() => {
-      if (!pause && slider) {
-        slider.next();
-      }
-    }, 2000);
-    return () => {
-      clearInterval(timer.current);
-    };
-  }, [pause, slider]);
 
   return (
     <>
-      <div className="navigation-wrapper">
-        <div ref={sliderRef} className="keen-slider">
-        <div class="image-additional keen-slider__slide number-slide1">
-                      <a href="#">
-                        <img
-                          src="https://opencart.mahardhi.com/MT04/noriva/02/image/cache/catalog/products/8-750x750.jpg"
-                          title="Zip-Front Jacket"
-                          alt="Zip-Front Jacket"
-                          width="80"
-                          height="80"
-                        />
-                      </a>
-                    </div>
-                    <div class="image-additional keen-slider__slide number-slide1">
-                      <a href="#">
-                        <img
-                          src="https://opencart.mahardhi.com/MT04/noriva/02/image/cache/catalog/products/1-750x750.jpg"
-                          title="Zip-Front Jacket"
-                          alt="Zip-Front Jacket"
-                          width="80"
-                          height="80"
-                        />
-                      </a>
-                    </div>
-                    <div class="image-additional keen-slider__slide number-slide1">
-                      <a href="#">
-                        <img
-                          src="https://opencart.mahardhi.com/MT04/noriva/02/image/cache/catalog/products/12-750x750.jpg"
-                          title="Zip-Front Jacket"
-                          alt="Zip-Front Jacket"
-                          width="80"
-                          height="80"
-                        />
-                      </a>
-                    </div>
-                    
-         
-          {/* <div className="keen-slider__slide number-slide1">
-            <a href="#">
-              <img
-                src="/images/mainbanner1-1920x900.jpg"
-                alt="Main-banner1"
-                className="img-responsive"
-              />
-            </a>
-          </div>
-          <div className="keen-slider__slide number-slide2">
-            <a href="#">
-              <img
-                src="/images/mainbanner2-1920x900.jpg"
-                alt="Main-banner2"
-                className="img-responsive"
-              />
-            </a>
-          </div>
-        */}
+      <div ref={sliderRef} className="keen-slider">
+        <div class="image-additional keen-slider__slide">
+          <a href="#">
+            <img
+              src="https://opencart.mahardhi.com/MT04/noriva/02/image/cache/catalog/products/8-750x750.jpg"
+              title="Zip-Front Jacket"
+              alt="Zip-Front Jacket"
+              width="80"
+              height="80"
+            />
+          </a>
         </div>
-        {slider && (
-          <>
-            <ArrowLeft
-              onClick={(e) => e.stopPropagation() || slider.prev()}
-              disabled={currentSlide === 0}
+        <div class="image-additional keen-slider__slide">
+          <a href="#">
+            <img
+              src="https://opencart.mahardhi.com/MT04/noriva/02/image/cache/catalog/products/8-750x750.jpg"
+              title="Zip-Front Jacket"
+              alt="Zip-Front Jacket"
+              width="80"
+              height="80"
             />
-            <ArrowRight
-              onClick={(e) => e.stopPropagation() || slider.next()}
-              disabled={currentSlide === slider.details().size - 1}
+          </a>
+        </div>
+        <div class="image-additional keen-slider__slide">
+          <a href="#">
+            <img
+              src="https://opencart.mahardhi.com/MT04/noriva/02/image/cache/catalog/products/8-750x750.jpg"
+              title="Zip-Front Jacket"
+              alt="Zip-Front Jacket"
+              width="80"
+              height="80"
             />
-          </>
-        )}
+          </a>
+        </div>
+        <div class="image-additional keen-slider__slide">
+          <a href="#">
+            <img
+              src="https://opencart.mahardhi.com/MT04/noriva/02/image/cache/catalog/products/8-750x750.jpg"
+              title="Zip-Front Jacket"
+              alt="Zip-Front Jacket"
+              width="80"
+              height="80"
+            />
+          </a>
+        </div>
       </div>
       {slider && (
-        <div className="dots">
-          {[...Array(slider.details().size).keys()].map((idx) => {
-            return (
-              <button
-                key={idx}
-                onClick={() => {
-                  slider.moveToSlideRelative(idx);
-                }}
-                className={"dot" + (currentSlide === idx ? " active" : "")}
-
-              />
-            );
-          })}
-        </div>
+        <>
+          <ArrowUp
+            onClick={(e) => e.stopPropagation() || slider.prev()}
+            // disabled={currentSlide === 0}
+          />
+          <ArrowDown
+            onClick={(e) => e.stopPropagation() || slider.next()}
+            // disabled={currentSlide === slider.details().size - 1}
+          />
+        </>
       )}
     </>
   );
 };
 
-function ArrowLeft(props) {
+// export default (props) => {
+//   const [currentSlide, setCurrentSlide] = React.useState(0);
+//   const [pause, setPause] = React.useState(false);
+//   const timer = React.useRef();
+
+//   const [sliderRef, slider] = useKeenSlider({
+//     initial: 0,
+//     loop: true,
+//     duration: 1000,
+//     dragStart: () => {
+//       setPause(true);
+//     },
+//     dragEnd: () => {
+//       setPause(false);
+//     },
+//     slideChanged(s) {
+//       setCurrentSlide(s.details().relativeSlide);
+//       console.log(s.details().relativeSlide);
+//     },
+//   });
+
+//   React.useEffect(() => {
+//     sliderRef.current.addEventListener("mouseover", () => {
+//       setPause(true);
+//     });
+//     sliderRef.current.addEventListener("mouseout", () => {
+//       setPause(false);
+//     });
+//   }, [sliderRef]);
+
+//   React.useEffect(() => {
+//     timer.current = setInterval(() => {
+//       if (!pause && slider) {
+//         slider.next();
+//       }
+//     }, 2000);
+//     return () => {
+//       clearInterval(timer.current);
+//     };
+//   }, [pause, slider]);
+
+//   return (
+//     <>
+//       <div class="product-additional">
+//         <div
+//           ref={sliderRef}
+//           id="additional-carousel"
+//           class="slick-carousel clearfix"
+//         >
+//           {/* <div className="navigation-wrapper">
+//         <div ref={sliderRef} className="keen-slider"> */}
+//           <div class="image-additional keen-slider__slide">
+//             <a href="#">
+//               <img
+//                 src="https://opencart.mahardhi.com/MT04/noriva/02/image/cache/catalog/products/8-750x750.jpg"
+//                 title="Zip-Front Jacket"
+//                 alt="Zip-Front Jacket"
+//                 width="80"
+//                 height="80"
+//               />
+//             </a>
+//           </div>
+//           <div class="image-additional keen-slider__slide">
+//             <a href="#">
+//               <img
+//                 src="https://opencart.mahardhi.com/MT04/noriva/02/image/cache/catalog/products/1-750x750.jpg"
+//                 title="Zip-Front Jacket"
+//                 alt="Zip-Front Jacket"
+//                 width="80"
+//                 height="80"
+//               />
+//             </a>
+//           </div>
+//           <div class="image-additional keen-slider__slide">
+//             <a href="#">
+//               <img
+//                 src="https://opencart.mahardhi.com/MT04/noriva/02/image/cache/catalog/products/12-750x750.jpg"
+//                 title="Zip-Front Jacket"
+//                 alt="Zip-Front Jacket"
+//                 width="80"
+//                 height="80"
+//               />
+//             </a>
+//           </div>
+
+//           {/* <div className="keen-slider__slide number-slide1">
+//             <a href="#">
+//               <img
+//                 src="/images/mainbanner1-1920x900.jpg"
+//                 alt="Main-banner1"
+//                 className="img-responsive"
+//               />
+//             </a>
+//           </div>
+//           <div className="keen-slider__slide number-slide2">
+//             <a href="#">
+//               <img
+//                 src="/images/mainbanner2-1920x900.jpg"
+//                 alt="Main-banner2"
+//                 className="img-responsive"
+//               />
+//             </a>
+//           </div>
+//         */}
+//         </div>
+//         {slider && (
+//           <>
+//             <ArrowLeft
+//               onClick={(e) => e.stopPropagation() || slider.prev()}
+//               disabled={currentSlide === 0}
+//             />
+//             <ArrowRight
+//               onClick={(e) => e.stopPropagation() || slider.next()}
+//               disabled={currentSlide === slider.details().size - 1}
+//             />
+//           </>
+//         )}
+//       </div>
+//       {slider && (
+//         <div className="dots">
+//           {[...Array(slider.details().size).keys()].map((idx) => {
+//             return (
+//               <button
+//                 key={idx}
+//                 onClick={() => {
+//                   slider.moveToSlideRelative(idx);
+//                 }}
+//                 className={"dot" + (currentSlide === idx ? " active" : "")}
+//               />
+//             );
+//           })}
+//         </div>
+//       )}
+//     </>
+//   );
+// };
+
+function ArrowUp(props) {
   const disabeld = props.disabled ? " arrow--disabled" : "";
   return (
     <svg
       onClick={props.onClick}
-      className={"arrow arrow--left" + disabeld}
+      className={"arrow arrow--up" + disabeld}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
     >
@@ -148,12 +224,12 @@ function ArrowLeft(props) {
   );
 }
 
-function ArrowRight(props) {
+function ArrowDown(props) {
   const disabeld = props.disabled ? " arrow--disabled" : "";
   return (
     <svg
       onClick={props.onClick}
-      className={"arrow arrow--right" + disabeld}
+      className={"arrow arrow--down" + disabeld}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
     >
