54bb7c446d99ce0b21c4bc2b8611979e21a6c793
cms <80998461+kieuoanh231@users.noreply.github.com>
2021-06-21 04:09:38 +0700

    login

--- store/GlobalState.js
+++ store/GlobalState.js
@@ -1,15 +1,32 @@
-import { createContext, useReducer } from "react";
-import reducers from './Reducers'
+import { createContext, useReducer, useEffect } from "react";
+import reducers from "./Reducers";
+import { getData } from "../utils/fetchData";
 
-export const DataContext=createContext()
-export const DataProvider=({children})=>{
-    const initialState={notify:{},auth:{}}
-    const [state,dispatch]=useReducer(reducers,initialState)
-    return (
-        <DataContext.Provider value={[state,dispatch]}>
-            {children}
-            {console.log(children)}
-            
-        </DataContext.Provider>
-    )
-}
+export const DataContext = createContext();
+export const DataProvider = ({ children }) => {
+  const initialState = { notify: {}, auth: {} };
+  const [state, dispatch] = useReducer(reducers, initialState);
+
+  useEffect(() => {
+    const firstLogin = localStorage.getItem("firstLogin");
+    if (firstLogin) {
+      getData("auth/accessToken").then((res) => {
+        if (res.err) {
+          return localStorage.removeItem("firstLogin");
+        }
+        dispatch({
+          type: "AUTH",
+          payload: {
+            toke: res.accessToken,
+            user: res.user,
+          },
+        });
+      });
+    }
+  }, []);
+  return (
+    <DataContext.Provider value={[state, dispatch]}>
+      {children}
+    </DataContext.Provider>
+  );
+};
