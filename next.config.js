6f777f219f4e71e86602d19f6c904261db0911bb
baongoc156 <68456260+baongoc156@users.noreply.github.com>
2021-06-22 16:51:44 +0700

    test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        "BASE_URL": "http://localhost:3000",
+        "BASE_URL": "https://project-on-fe.herokuapp.com/",
         "MONGODB_URL": "mongodb+srv://project_on:projecton123@cluster0.x9j0p.mongodb.net/project_on?retryWrites=true&w=majority",
         "ACCESS_TOKEN_SECRET":"nFWyb<)6~C$([U~}m6u5hPwt9[r7]2bM!q+#]{seY'P%?uGd~",
         "REFRESH_TOKEN_SECRET":"mnWjfZnZ}@*]cjvxApk*e4CeHWu(EH8,K)*j8y{+xfTXKLfxD$@!x4DH)T<a~<eNG#~7<FUSeC@B=p"
